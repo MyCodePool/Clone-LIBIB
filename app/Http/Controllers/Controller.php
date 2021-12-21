99d2f1d457af52ff5902c35f2b423ba790b0dc1d
bennohaus <m.cenk.toprak@gmail.com>
2021-12-21 10:09:58 +0100

    Controller Setup

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,19 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\Book;
+use App\Models\Photo;
+use App\Models\Tag;
+use App\Models\Taggable;
+use File;
+use DB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function showbooks()
+    {
+        return view('/');
+    }
 }
