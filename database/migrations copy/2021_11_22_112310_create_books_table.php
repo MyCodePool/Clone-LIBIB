56cc9d9fc16b5c749cb172f3822b115956ee6e7c
bennohaus <m.cenk.toprak@gmail.com>
2021-12-21 09:58:43 +0100

    Migrations

---
+++ database/migrations copy/2021_11_22_112310_create_books_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->string('title');
+            $table->text('summary')->nullable();
+
+            $table->date('release_date')->nullable();
+            $table->integer('price')->nullable();
+            $table->text('rate')->nullable();
+            $table->text('author')->nullable();
+            $table->text('distributor')->nullable();
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
