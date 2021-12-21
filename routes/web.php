af3fef2eb996b529cd245952ebfeaba6ed0a9de4
bennohaus <m.cenk.toprak@gmail.com>
2021-12-21 12:14:08 +0100

    Setting Routes
    Setting Routes for Design and Backend branch

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,18 @@ use App\Http\Controllers\Controller;
 
 # Routes & Params
 Route::get('/', [Controller::class, 'showbooks']);
+Route::get('/manage', [Controller::class, 'bookcreate']);
 
+
+# CRUD
+Route::resource('/bookpost', 'App\Http\Controllers\BooksController');
+Route::get('/manage/{id}', [Controller::class, 'bookupdate'])->name('bookupdate');
+Route::get('/delete/{id}', [Controller::class, 'bookdelete'])->name('bookdelete');
+
+# Filter 
+Route::get('/books/date/{date}', [Controller::class, 'show_books_by_date'])->name('show_books_by_date');
+Route::get('/books/tag/{tag}', [Controller::class, 'show_books_by_category'])->name('show_books_by_category');
+Route::get('/books/price/{range}', [Controller::class, 'show_books_by_pricerange'])->name('show_books_by_pricerange');
+Route::get('/books/rate/{rate}', [Controller::class, 'show_books_by_rate'])->name('show_books_by_rate');
+Route::get('/books/author/{author}', [Controller::class, 'show_books_by_author'])->name('show_books_by_author');
+Route::get('/books/distributor/{distributor}', [Controller::class, 'show_books_by_distributor'])->name('show_books_by_distributor');
