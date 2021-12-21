077fbc174cc925992cff35c47b29e635425b04d6
bennohaus <m.cenk.toprak@gmail.com>
2021-12-21 10:11:37 +0100

    Revert "Controller Setup"
    This reverts commit 99d2f1d457af52ff5902c35f2b423ba790b0dc1d.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,19 +6,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use App\Models\Book;
-use App\Models\Photo;
-use App\Models\Tag;
-use App\Models\Taggable;
-use File;
-use DB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function showbooks()
-    {
-        return view('/');
-    }
 }
