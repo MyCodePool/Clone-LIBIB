e3d038a4b1391065a6cd0b4ccba61f7fa8c8fd18
bennohaus <m.cenk.toprak@gmail.com>
2021-12-21 10:19:39 +0100

    Route to landing page
    Landing page shows all books in collection available

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('book');
-});
+# Routes for Library Books
+
+# Routes & Params
+Route::get('/', [Controller::class, 'showbooks']);
