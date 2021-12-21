15ddee47121b212d251d6e9666df311ae35703e6
bennohaus <m.cenk.toprak@gmail.com>
2021-12-21 10:03:51 +0100

    Models

---
+++ app/Models/Tag.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    public function posts(){
+
+        return $this->morphedByMany('App\Models\Post', 'taggable');
+
+    }
+
+    public function videos(){
+
+        return $this->morphedByMany('App\Models\Video', 'taggable');
+
+    }
+}
